284b5be0150434dbf305097a0c66e68201723c88
KitsuPlayz <nathankeijzer@outlook.com>
2024-11-18 12:50:42 +0100

    Color changing damage value

--- Assets/Devs/Terra/Scripts/MainGameUI.cs
+++ Assets/Devs/Terra/Scripts/MainGameUI.cs
@@ -6,22 +6,44 @@ using TMPro;
 public class MainGameUI : MonoBehaviour
 {
     [SerializeField] int CurrentRound = 0;
-    [SerializeField] int PlayerHealthPercentage = 0;
-    [SerializeField] int MaxDamage = 200;
     [SerializeField] GameObject RoundText;
     [SerializeField] GameObject Character1HealthText;
     [SerializeField] GameObject Character2HealthText;
     [SerializeField] GameObject Character3HealthText;
     [SerializeField] GameObject Character4HealthText;
+    [SerializeField] GameObject Character1cross, Character2cross, Character3cross, Character4cross;
+
+    //Temp stuff    
+    public bool P1elim = false;
+    public bool P2elim = false;
+    public bool P3elim = false;
+    public bool P4elim = false;
+    [SerializeField] float PlayerHealthPercentage = 0;
+    [SerializeField] int MaxDamage = 200;
 
 
     private void Update()
     {
         RoundText.GetComponent<TMP_Text>().text = "Round: " + CurrentRound.ToString();
         Character1HealthText.GetComponent<TMP_Text>().text = PlayerHealthPercentage.ToString() + "%";
-        Character1HealthText.GetComponent<TMP_Text>().color = Color.HSVToRGB(1, Mathf.Clamp(0, PlayerHealthPercentage, MaxDamage), 100);
+        Character1HealthText.GetComponent<TMP_Text>().color = Color.HSVToRGB(1, Mathf.Clamp(PlayerHealthPercentage, 1, 100) /100, 1);
         Character2HealthText.GetComponent<TMP_Text>().text = PlayerHealthPercentage.ToString() + "%";
         Character3HealthText.GetComponent<TMP_Text>().text = PlayerHealthPercentage.ToString() + "%";
         Character4HealthText.GetComponent<TMP_Text>().text = PlayerHealthPercentage.ToString() + "%";
+
+        if (PlayerHealthPercentage < 0)
+        {
+            PlayerHealthPercentage = 0;
+        }
+
+        if (PlayerHealthPercentage > 230)
+        {
+            PlayerHealthPercentage = 230;
+        }
+
+        if (P1elim)
+        {
+            Character1cross.SetActive(true);
+        }
     }
 }
